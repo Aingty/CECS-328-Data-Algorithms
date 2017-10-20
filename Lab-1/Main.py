d40d77f57ecc84724b0c2f541f53947a6f3304d8
Aingty Eung <aingtyeung@yahoo.com>
2017-10-20 15:41:45 -0700

    Finished lab 1 But still need to do part B

--- Lab-1/Main.py
+++ Lab-1/Main.py
@@ -8,7 +8,8 @@ from Search import *
 n = (int(input("\nEnter a positive number: ")))
 
 # Variables for logging search time
-bothTime = 0
+bothLinearTime = 0
+bothBinaryTime = 0
 
 array = [0] * n
 
@@ -19,25 +20,37 @@ for i in range(n):
 array.sort()
 
 # Calculating the timer to find exisitng number
-start = timeit.default_timer()
 for i in range(500):
     temp = random.randint(-1 , n-1)
     key = array[temp]
+
+    linearStartTime = timeit.default_timer()
     linearSearch(array, key)
+    linearEndTime = timeit.default_timer()
+    bothLinearTime = bothLinearTime + (linearEndTime - linearStartTime)
+
+    binaryStartTime = timeit.default_timer()
     binarySearch(array, key)
+    binaryEndTime = timeit.default_timer()
+    bothBinaryTime = bothBinaryTime + (binaryEndTime - binaryStartTime)
 
-end = timeit.default_timer()
 
-bothTime = (end - start)
 
+convertToMicrosecond = 1000000
+convertToMilisecond = 1000
 # Printing out the loop time
 print "\n********************* Part A *********************"
-print "Average Search Time: %.2f milisecond" %(round((bothTime/500)*1000, 2))
+print "Average Linear Search Time: %.2f milisecond" %(round((bothLinearTime/500)*1000, 2))
+print "Average Binary Search Time: %.2f microsecond" %(round((bothBinaryTime/500)*convertToMicrosecond, 2))
+
 
 # Calculating the timer to find non-existing number
 key = 5000
 
+
 linearSearch(array, key)
+
+
 start = timeit.default_timer()
 binarySearch(array, key)
 end = timeit.default_timer()
@@ -46,5 +59,5 @@ bothTime = (end - start)
 
 print "\n\n********************* Part B *********************"
 # Printing out all Time for searches
-print "One Step Time for n=%.0f : %.2f microsecond" %(n,round((bothTime)*1000000, 2))
-print "Estimate Time for n=10^7 : %.2f second" %(bothTime*10000000)
+print "One Step Time for n=%.0f : %.2f microsecond" %(n,round((bothTime)*convertToMicrosecond, 2))
+print "Estimate Time for n=10^7 : %.2f second\n" %(bothTime*10000000)
--- Lab-2/Sorting.py
+++ Lab-2/Sorting.py
@@ -1,18 +1,26 @@
 #!/usr/bin/env Python
 
-#import statistics
+import statistics
 
 def insertionSort(array):
     swap = 1
     while swap != 0:
         swap = 0
         for i in range(len(array)):
-            if (i+1 < len(array)):
+            if(i+1 < len(array)):
                 if(array[i] > array[i+1]):
                     array[i], array[i+1] = array[i+1], array[i]
                     swap = 1
     return array
 
-# def quickSort(array):
-#     medianList = [array[0], array[len(array)/2], array[len(array)]]
-#     statistics.median(medianList);
+def quickSort(array):
+    # if statement for base case
+    if(len(array) <= 3):
+        tempArray = [0] * len(array)
+        pivot = statistics.median(array)
+        for i in range(len(array)):
+                
+            
+            
+    medianList = [array[0], array[len(array)/2], array[len(array)]]
+    statistics.median(medianList)
