a8fa49911e8ec012d1228e66da4c388f2e455a6f
Aingty Eung <aingtyeung@yahoo.com>
2017-10-23 16:52:22 -0700

    Finished Lab 2

--- Lab-2/Main.py
+++ Lab-2/Main.py
@@ -4,7 +4,7 @@ import random
 from Sorting import *
 
 
-n = int(input("Please enter a positive number: "))
+n = int(input("\nPlease enter a positive number: "))
 
 array = [0] * n
 
@@ -12,5 +12,8 @@ for i in range(n):
     l = random.randint(-7000 , 7000)
     array[i] = l
 
-array = insertionSort(array)
-print ("Insertion Sort: %s" %array)
+array1 = insertionSort(array)
+print ("\nInsertion Sort: %s \n" %array)
+
+array2 = quickSort(array)
+print ("Quick Sort: %s \n" %array)
--- Lab-2/Sorting.py
+++ Lab-2/Sorting.py
@@ -16,15 +16,26 @@ def insertionSort(array):
 def quickSort(array):
     # if statement for base case
     if(len(array) <= 3):
-        swap = 0
-        while swap != 0:
-            swap = 0    
-            for i in range(len(array)):
-                if(i+1 < len(array)):
-                    if(array[i] > array[i+1]):
-                        array[i], array[i+1] = array[i+1], array[i]
-                        swap = 1
+        # swap = 0
+        # while swap != 0:
+        #     swap = 0    
+        #     for i in range(len(array)):
+        #         if(i+1 < len(array)):
+        #             if(array[i] > array[i+1]):
+        #                 array[i], array[i+1] = array[i+1], array[i]
+        #                 swap = 1
+        return insertionSort(array)
+    else:
+        left = []
+        right = []
+        medianList = [array[0], array[(int)(len(array)/2)], array[len(array)-1]]
+        pivot = statistics.median(medianList)
+        for i in range(len(array)):
+            if (array[i] <= pivot):
+                left.append(array[i])
+            else:
+                right.append(array[i])
+        left = quickSort(left)
+        right = quickSort(right)
+        return (left + right)        
             
-            
-    medianList = [array[0], array[len(array)/2], array[len(array)]]
-    statistics.median(medianList)
