85d78cf51888b03d0964fe32d2ff7974aba3a76c
Aingty Eung <aingtyeung@yahoo.com>
2017-11-03 09:54:12 -0700

    Finished Lab 2 and Started hw 4 Need debugging for lab 2

--- Lab-2/Main.py
+++ Lab-2/Main.py
@@ -2,22 +2,41 @@
 
 import random
 import sys
+import timeit
 from Sorting import *
+sys.setrecursionlimit(150000)
 
 
 n = int(input("\nPlease enter a positive number: "))
 
 array = [0] * n
 
+insertionTime = 0
+quickTime = 0
 for i in range(n):
     l = random.randint(-7000 , 7000)
     array[i] = l
 
-sys.setrecursionlimit(1500)
-
 
 array1 = insertionSort(array)
-print ("\nInsertion Sort: %s \n" %array)
 
-array2 = quickSort(array)
-print ("Quick Sort: %s \n" %array)
+print("\nThe array: %s" %array1)
+
+
+# for i in range(100):
+    
+#     start = timeit.default_timer()
+#     array1 = insertionSort(array)
+#     end = timeit.default_timer()
+#     insertionTime = insertionTime + (end - start)
+    
+#     start = timeit.default_timer()
+#     array2 = quickSort(array)
+#     end = timeit.default_timer()
+#     quickTime = quickTime + (end - start)
+
+# insertionTime = insertionTime/100
+# quickTime = quickTime/100
+
+# print ("\nAverage Insertion Sort Time: %f \n" %insertionTime)
+# print ("Average Quick Sort Time: %f \n" %quickTime)
