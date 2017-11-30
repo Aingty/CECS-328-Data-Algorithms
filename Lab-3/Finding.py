9cdcc7d94d094e9180190fe279ae6767f985f02e
Aingty Eung <aingtyeung@yahoo.com>
2017-11-29 17:00:56 -0800

    Lab 3 in Progress

---
+++ Lab-3/Finding.py
@@ -0,0 +1,23 @@
+# Function to find the kth least element
+
+def findingK(array, k):
+    # First element to be pivot
+    tempArray = array
+    left = []
+    right = []
+    kNotFound = True
+    while kNotFound:
+        pivot = tempArray[0]
+        for i in range(len(tempArray)):
+            if(tempArray[i] < pivot):
+                left.append(tempArray[i])
+            else:
+                right.append(tempArray[i])
+        left.append(pivot)
+        tempArray = []
+        # Check if K-value is left or right
+        if k < len(left):
+            tempArray = left
+        elif k > len(left):
+            tempArray = right
+        else:
---
+++ Lab-3/Main.py
@@ -0,0 +1,27 @@
+
+import random
+
+keepGoing = True
+while keepGoing:
+    n = input("\nPlease input the array size: ")
+    if n.isdigit():
+        n = int(n)
+        array = [0] * n
+        for i in range(n):
+            l = random.randint(-100,100)
+            array[i] = l
+        print("Generated Array: \n%s\n" %array)
+        invalidK = True
+        while invalidK:
+            k = input("Please input a number from 1 to %.0f as k least element: " %n) 
+            if k.isdigit():
+                k = int(k)
+                if k >= 1 and k <= n:
+                    invalidK = False
+                else:
+                    print("Invalid K-value, please input within range!!")
+            else:
+                print("Invalid K-value, please input a number!!")
+                    
+    else:
+        print("Incorrect array size value!!!")
