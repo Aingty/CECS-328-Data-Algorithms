461c9d0c3e8b1a4eec5ab8623e81be264fb194d2
Aingty Eung <aingtyeung@yahoo.com>
2017-12-01 00:48:54 -0800

    Uncommented Important Section

--- Lab-4/Main.py
+++ Lab-4/Main.py
@@ -6,13 +6,12 @@ while keepGoing:
     n = input("\nPlease input the array size: ")
     if n.isdigit():
         n = int(n)
-
         # array1 = [-19,-7,-12,-4,-50,-5,-10,-2]
-        array1 = [19,-7,12,4,-50,5,10,-2]
-        # array1 = [0] * n
-        # for i in range(n):
-        #     l = random.randint(-100,100)
-        #     array1[i] = l
+        # array1 = [19,-7,12,4,-50,5,10,-2]
+        array1 = [0] * n
+        for i in range(n):
+            l = random.randint(-100,100)
+            array1[i] = l
     else:
         print("Incorrect array size value!!")
         continue
